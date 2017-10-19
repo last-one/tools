d82a5bc2df42e0afb985da96d534483d10dfe036
last_one <last_onephy@gmail.com>
2017-10-19 14:57:10 +0800

    add label shuffling

--- pytorch/BasicTool.py
+++ pytorch/BasicTool.py
@@ -3,10 +3,42 @@ import torch
 import shutil
 import time
 import os
+import random
 from PIL import Image
 from easydict import EasyDict as edict
 import yaml
 import torch.utils.data as data
+import numpy as np
+
+# 
+
+#
+def read_data_file_with_dict(filename, root=None):
+
+    lists = []
+    dicts = {}
+    cnt = 0
+    with open(filename, 'r') as fp:
+        line = fp.readline()
+        while line:
+            info = line.strip().split(' ')
+            if root is not None:
+                #info[0] = root + info[0]
+                info[0] = os.path.join(root, info[0])
+            item = (info[0], int(info[1]))
+            lists.append(item)
+
+            if int(info[1]) not in dicts:
+                dicts[int(info[1])] = []
+            dicts[int(info[1])].append(cnt)
+            cnt += 1
+
+            line = fp.readline()
+
+    max_number = 0
+    for x in dicts:
+        max_number = max(max_number, len(dicts[x]))
+    return lists, dicts, max_number
 
 def read_data_file(filename, root=None):
 
@@ -16,6 +48,7 @@ def read_data_file(filename, root=None):
         while line:
             info = line.strip().split(' ')
             if root is not None:
+                #info[0] = root + info[0]
                 info[0] = os.path.join(root, info[0])
             item = (info[0], int(info[1]))
             lists.append(item)
@@ -28,6 +61,22 @@ def pil_loader(path):
     with open(path, 'rb') as f:
         with Image.open(f) as img:
             return img.convert('RGB')
+
+def label_shuffle(dicts, max_number, idx):
+
+    index = [i for i in range(max_number)]
+    cnt = 0
+
+    for x in dicts:
+        info = dicts[x]
+        print info
+        random.shuffle(info)
+        length = len(info)
+        random.shuffle(index)
+        for y in index:
+            ids = y % length
+            idx[cnt] = info[ids]
+            cnt += 1
 
 class MydataFolder(data.Dataset):
     """A data loader where the list is arranged in this way:
@@ -50,11 +99,18 @@ class MydataFolder(data.Dataset):
         root (string): Root directory path.
         transform (callable, optional): A function/transform that takes in an PIL image
             and returns a transformed version
+        label_shuffling: label shuffle or not. default is False. If want to use label suffling, please modify the dataloader.py, 
+                            in the __iter__(), add as follow:
+                                        'if callable(self.dataset.__shuffle__):
+                                            self.dataset.__shuffle__()'
     """
 
-    def __init__(self, filename, root=None, transform=None):
+    def __init__(self, filename, root=None, transform=None, label_shuffling=False):
 
-        lists = read_data_file(filename, root)
+        if label_shuffling:
+            lists, dicts, max_number = read_data_file_with_dict(filename, root)
+        else:
+            lists = read_data_file(filename, root)
         if len(lists) == 0:
             raise(RuntimeError('Found 0 images in subfolders\n'))
 
@@ -62,6 +118,20 @@ class MydataFolder(data.Dataset):
         self.transform = transform
         self.lists = lists
         self.load = pil_loader
+        self.label_shuffling = label_shuffling
+
+        if self.label_shuffling:
+            self.dicts = dicts
+            self.max_number = max_number
+            self.idx = [i for i in range(max_number * len(self.dicts))]
+        else:
+            self.idx = [i for i in range(len(self.lists))]
+
+    def __shuffle__(self):
+
+        if self.label_shuffling:
+            label_shuffle(self.dicts, self.max_number, self.idx)
+            print self.idx
 
     def __getitem__(self, index):
         """
@@ -72,7 +142,7 @@ class MydataFolder(data.Dataset):
             tuple: (image, label) where label is the clas of the image
         """
 
-        path, label = self.lists[index]
+        path, label = self.lists[self.idx[index]]
         img = self.load(path)
 
         if self.transform is not None:
@@ -82,7 +152,7 @@ class MydataFolder(data.Dataset):
 
     def __len__(self):
         
-        return len(self.lists)
+        return len(self.idx)
 
 class AverageMeter(object):
     """ Computes ans stores the average and current value"""
@@ -155,4 +225,6 @@ def Config(filename):
 
     with open(filename, 'r') as f:
         parser = edict(yaml.load(f))
+    for x in parser:
+        print '{}: {}'.format(x, parser[x])
     return parser
